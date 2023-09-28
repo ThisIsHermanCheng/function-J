5b42875126b8bcde803f0ce0022174f97ea590a2
Herman Cheng <cheng0505@outlook.com>
2023-09-28 06:41:14 +0000

    Refactor command execution and input handling
    - Improved error handling in executeCommand function
    - Added input handling for commands with placeholders
    - Display prompts and replace placeholders with user input

--- process.js
+++ process.js
@@ -8,17 +8,58 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-// process.stdout.write(`\x1B[?25l`);
+process.stdout.write(`\x1B[?25l`);
 process.on('exit', () => process.stdout.write('\x1B[?25h\n'));
 
 keypress(process.stdin);
 process.stdin.setRawMode(true);
 
+let inputting = false
+
 process.stdin.on('keypress', (_, key) => {
     if (!key) return
     if (key.name === 'tab') setIndex((options.index + 1) % 3)
     else if (key.name === 'return') {
-        rl.close();
-        executeCommand(options.selected)
+
+        if (inputting) return
+
+        const regex = /\{([^}]+)\}/g;
+        const matches = [];
+        let match;
+        while ((match = regex.exec(options.selected)) !== null) {
+            matches.push(match[1]);
+        }
+
+        if (matches.length == 0) {
+            rl.close();
+            executeCommand(options.selected)
+            inputting = false
+        } else {
+            console.log("-------------------------------------------------------")
+            console.log("Please input the following values")
+
+            inputting = true
+            currentIndex = 0
+
+            function askQuestion() {
+                rl.question(matches[currentIndex] + ': ', (answer) => {
+                    options.selected = options.selected.replace(`{${matches[currentIndex]}}`, answer)
+                    currentIndex++;
+                    if (currentIndex < matches.length) {
+                        askQuestion();
+                    } else {
+                        executeCommand(options.selected)
+                        rl.close();
+                    }
+                });
+            }
+
+
+            askQuestion();
+
+        }
+
+
     }
 });
+
