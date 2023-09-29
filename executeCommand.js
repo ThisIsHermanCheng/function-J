2bb2e49a4c0f8e8a069fb3f925aaa241d7dad860
Herman Cheng <cheng0505@outlook.com>
2023-09-29 06:20:44 +0000

    add repo info

--- executeCommand.js
+++ executeCommand.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 function executeCommand(command) {
     console.log(`-------------------------------------------------------\n`);
     exec(`bash -c '${command}'`, (error, stdout, _) => {
-        if (error) return console.log('222\x1b[31m%s\x1b[0m', error.stack);
+        if (error) return console.log('\x1b[31m%s\x1b[0m', error.stack);
         console.log(`${stdout}`);
     });
 }
